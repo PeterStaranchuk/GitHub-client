6ebc8894e35e24209ef3d72fa0d99b1c228d4920
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-02 00:43:39 +0300

    Add RepoModel to retrieve repositories from GitHub api Add ViewModel to manage state of screen and get info from RepoModel

---
+++ app/src/main/java/peter/staranchuk/githubclient/model/RepoModel.kt
@@ -0,0 +1,18 @@
+package peter.staranchuk.githubclient.model
+
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import peter.staranchuk.githubclient.network.GitHubApi
+import peter.staranchuk.githubclient.network.response.RepositoryInfo
+
+class RepoModel {
+
+    fun getRepositories(searchQuery: String, success: (repositories: List<RepositoryInfo>) -> Unit, failure: (message: String?) -> Unit) {
+        GitHubApi.Factory().getInstance().getRepositories(searchQuery)
+                .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe({ response ->
+                    success(response.items)
+                }, {
+                    failure(it.message)
+                })
+    }
+}
---
+++ app/src/main/java/peter/staranchuk/githubclient/screen_main/ViewModelMain.kt
@@ -0,0 +1,28 @@
+package peter.staranchuk.githubclient.screen_main
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import peter.staranchuk.githubclient.model.RepoModel
+import peter.staranchuk.githubclient.network.response.RepositoryInfo
+
+
+class ViewModelMain : ViewModel() {
+    private var repoModel = RepoModel()
+    var repositories = MutableLiveData<List<RepositoryInfo>>()
+    var isLoading = ObservableField<Boolean>(false)
+    var isLoadingError = ObservableField<Boolean>(false)
+
+    fun refreshData(searchQuery : String) {
+        isLoading.set(true)
+        isLoadingError.set(false)
+
+        repoModel.getRepositories(searchQuery, {newRepositories ->
+            isLoading.set(false)
+            repositories.value = newRepositories
+        }, {
+            isLoading.set(false)
+            isLoadingError.set(true)
+        })
+    }
+}
