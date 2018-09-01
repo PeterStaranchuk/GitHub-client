3e03a3ca741bc19b530498f95d4ffe41b1da161d
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-02 00:42:16 +0300

    Add MainActivity layout, binding for data/view

---
+++ app/src/main/java/peter/staranchuk/githubclient/screen_main/MainActivity.kt
@@ -0,0 +1,47 @@
+package peter.staranchuk.githubclient.screen_main
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import peter.staranchuk.githubclient.R
+import peter.staranchuk.githubclient.adapters.RepoAdapter
+import peter.staranchuk.githubclient.databinding.ActivityMainBinding
+import peter.staranchuk.githubclient.network.response.RepositoryInfo
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val searchStartDelay = 300L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val viewModel = ViewModelProviders.of(this).get(ViewModelMain::class.java)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.executePendingBindings()
+
+        binding.rvRepositories.layoutManager = LinearLayoutManager(this)
+
+        viewModel.repositories.observe(this, Observer<List<RepositoryInfo>> { newRepositoriesList ->
+            newRepositoriesList?.let {
+                rvRepositories.adapter = RepoAdapter(it) { clickedItemPosition ->
+                    //TODO add on item clicked handler
+                }
+            }
+        })
+
+        RxTextView.afterTextChangeEvents(etSearchQuery).skipInitialValue()
+                .debounce(searchStartDelay, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { viewModel.refreshData(etSearchQuery.text.toString().trim()) }
+    }
+}
