f676d1c926f2107dcea8d47bda804243e7cd5760
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-09 14:57:06 +0300

    Reuse UserInfo instead of Owner class because it is the same objects

--- app/src/main/java/peter/staranchuk/githubclient/network/response/GitHubReposResponse.kt
+++ app/src/main/java/peter/staranchuk/githubclient/network/response/GitHubReposResponse.kt
@@ -6,8 +6,7 @@ import peter.staranchuk.githubclient.interfaces.GitHubItem
 
 data class GitHubReposResponse(@SerializedName("items") val repos: List<RepositoryInfo>)
 
-data class RepositoryInfo(val name: String, val description: String, val owner: Owner) : GitHubItem {
+data class RepositoryInfo(val name: String, val description: String, val owner: UserInfo) : GitHubItem {
     override fun getCode(): Int = GitHubItemType.REPOSITORY.code
 }
 
-data class Owner(@SerializedName("avatar_url") val avatarUrl: String, val login: String)
